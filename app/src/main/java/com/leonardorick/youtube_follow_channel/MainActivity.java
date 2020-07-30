73c2a48e8b7b9b5f8e899a059533522823555f0a
Leonardo Rick <leonardo@lanytecnologia.com.br>
2020-07-29 22:21:15 -0300

    Initial commit

---
+++ app/src/main/java/com/leonardorick/youtube_follow_channel/MainActivity.java
@@ -0,0 +1,16 @@
+package com.leonardorick.youtube_follow_channel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String GOOGLE_API_KEY = "AIzaSyCk-Dq3sYzBEsb9CEb_KzJglXlfK5bLFmE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
